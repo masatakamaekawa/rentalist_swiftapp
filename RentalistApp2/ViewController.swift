77a13cfdd8b4e210944e5467590b9e55d89d9cea
masatakamaekawa <mstkmekw@hito-koto.com>
2021-11-21 19:40:40 +0900

    Initial Commit

---
+++ RentalistApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentalistApp2
+//
+//  Created by masataka maekawa on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
